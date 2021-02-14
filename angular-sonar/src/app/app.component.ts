dbc2a656043dfe939bdfe38451df328985a25960
fitzhi <frederic.vidal@fitzhi.com>
2021-02-14 08:16:04 +0100

    Retrieving the Sonar version

--- angular-sonar/src/app/app.component.ts
+++ angular-sonar/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular and Sonar dialog';
+
+  public sonarQubeServer = 'http://localhost:9000';
+
+
+  constructor(private httpClient : HttpClient) {}
+
+  public go() {
+
+    this.initSonarServer('http://localhost:9000');
+		const subscription = this.httpClient
+			.get(this.sonarQubeServer + '/api/server/version', {responseType: 'text' as 'json' })
+        .subscribe({
+          next: (version: string) => console.log (version) });
+  }
+
+	/**
+	 * Initialize the `SonarServer` object for future usage, and tests its avaibility by retrieving its version.
+	 * @param urlSonar the URL of the Sonar server
+	 */
+	private initSonarServer(urlSonar: string) {
+		const subscription = this.httpClient
+			.get(urlSonar + '/api/server/version', { responseType: 'text' as 'json' })
+				.subscribe({
+					next: result => console.log (result),
+					complete: () => setTimeout(() => { subscription.unsubscribe(); } , 0)
+				});
+	}
+
+
 }
